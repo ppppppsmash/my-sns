b8b3ec8de01c8a5e579e512972db2242a4d3033c
ppppppsmash <pppppp.work@gmail.com>
2023-11-23 15:28:13 +0900

    feat: User Model for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  # /users/sign_up
   devise_for :users
+
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
