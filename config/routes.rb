a9a4291ce077d539a9fc504e52e92f78f91db0c1
ppppppsmash <pppppp.work@gmail.com>
2023-12-02 23:47:40 +0900

    feat: Likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'home#index'
+
+  post 'toggle_like', to: 'likes#toggle_like', as: :toggle_like
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
